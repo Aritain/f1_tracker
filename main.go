0cc74454ede11ad4533786e6a2c04b77a88c1b78
aritain <ganhart2@gmail.com>
2023-06-12 19:56:24 +0200

    spacing fixes

--- main.go
+++ main.go
@@ -3,31 +3,30 @@ package main
 import (
     "github.com/Syfaro/telegram-bot-api"
     "log"
-	"os"
+    "os"
     "client/client"
 )
 
 
 func main() {
 
-	
-	_, status := os.LookupEnv("TG_TOKEN")
-	if status == false {
-		log.Printf("TG_TOKEN env is missing.")
-		os.Exit(1)
-	}
+    _, status := os.LookupEnv("TG_TOKEN")
+    if status == false {
+        log.Printf("TG_TOKEN env is missing.")
+        os.Exit(1)
+    }
 
     bot, err := tgbotapi.NewBotAPI(os.Getenv("TG_TOKEN"))
-	if err != nil {
-		log.Panic(err)
-	}
+    if err != nil {
+        log.Panic(err)
+    }
     bot.Debug = true
 
-	// Create chan for telegram updates
-	var ucfg tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
-	ucfg.Timeout = 60
-	updates, _ := bot.GetUpdatesChan(ucfg)
-	for update := range updates {
+    // Create chan for telegram updates
+    var ucfg tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
+    ucfg.Timeout = 60
+    updates, _ := bot.GetUpdatesChan(ucfg)
+    for update := range updates {
         if update.Message == nil { // ignore any non-Message updates
             continue
         }
@@ -36,10 +35,10 @@ func main() {
             continue
         }
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-		log.Printf("%s", update.Message.Command())
-		msg.Text = client.FetchData(update.Message.Command())
-		msg.ParseMode = "markdown"
+        msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+        log.Printf("%s", update.Message.Command())
+        msg.Text = client.FetchData(update.Message.Command())
+        msg.ParseMode = "markdown"
 
 
         if _, err := bot.Send(msg); err != nil {
